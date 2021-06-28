3bab41d6b52ddc5585a2146e866b65ea211920f5
Luis Costilla <luis287@gmail.com>
2021-06-28 09:39:59 -0400

    Add project files.

---
+++ TheBestPainters/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBestPainters.Startup))]
+namespace TheBestPainters
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
