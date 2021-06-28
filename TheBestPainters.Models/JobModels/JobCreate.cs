690b9888fe707ce089051e69ef25989174e58735
Luis Costilla <luis287@gmail.com>
2021-06-28 15:24:40 -0400

    JobCreate added in Job Models

---
+++ TheBestPainters.Models/JobModels/JobCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.JobModels
+{
+    public class JobCreate
+    {
+        [Required]
+        public int? CustomerId { get; set; }
+        [Required]
+        public string JobLocation { get; set; }
+        [Required]
+        public string ScopeOfWork { get; set; }
+        public bool Interior { get; set; }
+        public bool Exterior { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
