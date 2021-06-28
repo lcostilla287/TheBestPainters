87ae9940bb586e5a437e3c95ceb676f9bfcbc5ff
Luis Costilla <luis287@gmail.com>
2021-06-28 16:39:26 -0400

    Job service CRUD methods completed

--- TheBestPainters.Services/JobService.cs
+++ TheBestPainters.Services/JobService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheBestPainters.Data;
+using TheBestPainters.Models.JobModels;
 
 namespace TheBestPainters.Services
 {
@@ -13,6 +15,106 @@ namespace TheBestPainters.Services
         public JobService(Guid userId)
         {
             _userId = userId;
+        }
+
+        public bool CreateJob(JobCreate model)
+        {
+            var entity =
+                new Job()
+                {
+                    OwnerId = _userId,
+                    JobLocation = model.JobLocation,
+                    ScopeOfWork = model.ScopeOfWork,
+                    Interior = model.Interior,
+                    Exterior = model.Exterior,
+                    CustomerId = model.CustomerId
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Jobs.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<JobListItem> GetJobs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Jobs
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new JobListItem
+                            {
+                                JobId = e.JobId,
+                                CustomerId = e.CustomerId,
+                                JobLocation = e.JobLocation,
+                                Interior = e.Interior,
+                                Exterior = e.Exterior
+                            }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public JobDetail GetJobById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Jobs
+                        .Single(e => e.JobId == id && e.OwnerId == _userId);
+                return
+                    new JobDetail
+                    {
+                        JobId = entity.JobId,
+                        CustomerId = entity.CustomerId,
+                        FullName = entity.Customer.FullName,
+                        JobLocation = entity.JobLocation,
+                        Interior = entity.Interior,
+                        Exterior = entity.Exterior,
+                        Price = entity.Price
+                    };
+            }
+        }
+
+        public bool UpdateJob(JobEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Jobs
+                        .Single(e => e.JobId == model.JobId && e.OwnerId == _userId);
+
+                entity.JobLocation = model.JobLocation;
+                entity.CustomerId = model.CustomerId;
+                entity.ScopeOfWork = model.ScopeOfWork;
+                entity.Interior = model.Interior;
+                entity.Exterior = model.Exterior;
+                entity.Price = model.Price;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteJob(int jobId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Jobs
+                        .Single(e => e.JobId == jobId && e.OwnerId == _userId);
+
+                ctx.Jobs.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
         }
     }
 }
