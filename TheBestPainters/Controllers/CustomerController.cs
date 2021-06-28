ab09c5cb6b0780fd69a9fe1217a96d1cbc0fa178
Luis Costilla <luis287@gmail.com>
2021-06-28 11:50:11 -0400

    modified create in customer controller and added bootstrap error message

--- TheBestPainters/Controllers/CustomerController.cs
+++ TheBestPainters/Controllers/CustomerController.cs
@@ -32,17 +32,26 @@ namespace TheBestPainters.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(CustomerCreate model)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateCustomerService();
+
+            if (service.CreateCustomer(model))
             {
-                return View(model);
+                TempData["SaveResult"] = "Your Customer was added.";
+                return RedirectToAction("Index");
             }
 
+            ModelState.AddModelError("", "Customer could not be created.");
+
+            return View(model);
+        }
+
+        private CustomerService CreateCustomerService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new CustomerService(userId);
-
-            service.CreateCustomer(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
