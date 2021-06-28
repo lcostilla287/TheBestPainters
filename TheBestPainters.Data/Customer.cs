8dca5394cbad6e229c69030e40b530735ddad9a2
Luis Costilla <luis287@gmail.com>
2021-06-28 10:10:29 -0400

    Customer class added in models

---
+++ TheBestPainters.Data/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string CityAddress { get; set; }
+
+        public string Email { get; set; }
+    }
+}
