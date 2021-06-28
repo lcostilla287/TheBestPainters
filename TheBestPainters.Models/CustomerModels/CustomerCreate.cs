6b450874e68620070997b139c8e2364ccefe0799
Luis Costilla <luis287@gmail.com>
2021-06-28 11:11:05 -0400

    added view for customer create and customer create model

---
+++ TheBestPainters.Models/CustomerModels/CustomerCreate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.CustomerModels
+{
+    public class CustomerCreate
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string CityAddress { get; set; }
+
+        public string Email { get; set; }
+    }
+}
--- TheBestPainters/Controllers/CustomerController.cs
+++ TheBestPainters/Controllers/CustomerController.cs
@@ -16,5 +16,22 @@ namespace TheBestPainters.Controllers
             var model = new CustomerListItem[0];
             return View(model);
         }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
