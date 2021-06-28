c96665719a702cd950d5778266dec24f71d0b4b9
Luis Costilla <luis287@gmail.com>
2021-06-28 11:22:14 -0400

    Added create customer and Get customers in customer service

---
+++ TheBestPainters.Services/CustomerService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBestPainters.Data;
+using TheBestPainters.Models.CustomerModels;
+
+namespace TheBestPainters.Services
+{
+    public class CustomerService
+    {
+        private readonly Guid _userId;
+
+        public CustomerService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCustomer(CustomerCreate model)
+        {
+            var entity =
+                new Customer()
+                {
+                    OwnerId = _userId,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    StreetAddress = model.StreetAddress,
+                    CityAddress = model.CityAddress,
+                    Email = model.Email
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Customers.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CustomerListItem> GetCustomers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Customers
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new CustomerListItem
+                                {
+                                    CustomerId = e.CustomerId,
+                                    FullName = e.FullName,
+                                    PhoneNumber = e.PhoneNumber
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
