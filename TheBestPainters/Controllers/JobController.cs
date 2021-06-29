057da7d50839e1545aaa2465d3a81e002ef2c945
Luis Costilla <luis287@gmail.com>
2021-06-29 09:24:45 -0400

    Migrated changes to jobs and added index view to job

---
+++ TheBestPainters/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheBestPainters.Services;
+
+namespace TheBestPainters.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new JobService(userId);
+            var model = service.GetJobs();
+
+            return View(model);
+        }
+    }
+}
