8154b8340dabad8e8ed1938ee3d566cfca8a96af
Luis Costilla <luis287@gmail.com>
2021-06-29 12:01:55 -0400

    Material Detail Added in Models

---
+++ TheBestPainters.Models/MaterialModels/MaterialDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.MaterialModels
+{
+    public class MaterialDetail
+    {
+        public int MaterialId { get; set; }
+        public int? JobId { get; set; }
+        public string MaterialName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
