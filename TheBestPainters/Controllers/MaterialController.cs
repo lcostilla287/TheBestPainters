a051a45ca8ed5b4c95be30665f9a4d16c8683777
Luis Costilla <luis287@gmail.com>
2021-06-29 14:02:02 -0400

    Create methods completed in Material controller, Material Index adjusted to have TempData

--- TheBestPainters/Controllers/MaterialController.cs
+++ TheBestPainters/Controllers/MaterialController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheBestPainters.Models.MaterialModels;
 using TheBestPainters.Services;
 
 namespace TheBestPainters.Controllers
@@ -19,6 +20,29 @@ namespace TheBestPainters.Controllers
             return View(model);
         }
 
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MaterialCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateMaterialService();
+
+            if (service.CreateMaterial(model))
+            {
+                TempData["SaveResult"] = "The Material was added.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Material could not be added.");
+            return View(model);
+        }
         private MaterialService CreateMaterialService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
