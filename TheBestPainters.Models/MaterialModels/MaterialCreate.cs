7469c5d4996d85e5e106ed33f92eaf8075885507
Luis Costilla <luis287@gmail.com>
2021-06-29 11:55:16 -0400

    Material Create added in Material Models

---
+++ TheBestPainters.Models/MaterialModels/MaterialCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.MaterialModels
+{
+    public class MaterialCreate
+    {
+        public int? JobId { get; set; }
+
+        [Required]
+        public string MaterialName { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+
+    }
+}
