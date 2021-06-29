4130247e77e8544e65cc71cdc22354ccf872401f
Luis Costilla <luis287@gmail.com>
2021-06-29 10:12:53 -0400

    Created Views for Job

--- TheBestPainters/Controllers/JobController.cs
+++ TheBestPainters/Controllers/JobController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheBestPainters.Models.JobModels;
 using TheBestPainters.Services;
 
 namespace TheBestPainters.Controllers
@@ -18,6 +19,110 @@ namespace TheBestPainters.Controllers
             var model = service.GetJobs();
 
             return View(model);
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(JobCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateJobService();
+
+            if (service.CreateJob(model))
+            {
+                TempData["SaveResult"] = "The Job was added.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Job could not be created");
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateJobService();
+            var model = svc.GetJobById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateJobService();
+            var detail = service.GetJobById(id);
+            var model =
+                new JobEdit
+                {
+                    JobId = detail.JobId,
+                    CustomerId = detail.CustomerId,
+                    JobLocation = detail.JobLocation,
+                    ScopeOfWork = detail.ScopeOfWork,
+                    Interior = detail.Interior,
+                    Exterior = detail.Exterior,
+                    Price = detail.Price
+                };
+            return View(model);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, JobEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.JobId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateJobService();
+
+            if (service.UpdateJob(model))
+            {
+                TempData["SaveResult"] = "Your Customer was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The job could not be updated.");
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateJobService();
+            var model = svc.GetJobById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteJob(int id)
+        {
+            var service = CreateJobService();
+
+            service.DeleteJob(id);
+
+            TempData["SaveResult"] = "The was deleted";
+
+            return RedirectToAction("Index");
+        }
+        private JobService CreateJobService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new JobService(userId);
+            return service;
         }
     }
 }
