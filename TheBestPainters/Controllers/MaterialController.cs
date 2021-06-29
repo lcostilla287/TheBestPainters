6836327f9e5aefee681329e684db250975bb2f15
Luis Costilla <luis287@gmail.com>
2021-06-29 13:53:52 -0400

    Index added in material controller and view

---
+++ TheBestPainters/Controllers/MaterialController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheBestPainters.Services;
+
+namespace TheBestPainters.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            var service = CreateMaterialService();
+            var model = service.GetMaterials();
+
+            return View(model);
+        }
+
+        private MaterialService CreateMaterialService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new MaterialService(userId);
+            return service;
+        }
+    }
+}
