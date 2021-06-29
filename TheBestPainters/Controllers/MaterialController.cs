25af35f4f9e2b0f8dffaebfecf9fe45ab2f9f2c7
Luis Costilla <luis287@gmail.com>
2021-06-29 14:13:06 -0400

    Edit view added in Materials, Edit methods added in material controller

--- TheBestPainters/Controllers/MaterialController.cs
+++ TheBestPainters/Controllers/MaterialController.cs
@@ -43,6 +43,54 @@ namespace TheBestPainters.Controllers
             ModelState.AddModelError("", "Material could not be added.");
             return View(model);
         }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateMaterialService();
+            var model = svc.GetMaterialById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateMaterialService();
+            var detail = service.GetMaterialById(id);
+            var model =
+                new MaterialEdit
+                {
+                    MaterialId = detail.MaterialId,
+                    MaterialName = detail.MaterialName,
+                    JobId = detail.JobId,
+                    Price = detail.Price,
+                    Quantity = detail.Quantity
+                };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, MaterialEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.MaterialId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateMaterialService();
+
+            if (service.UpdateMaterial(model))
+            {
+                TempData["SaveResult"] = "The Material was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The Material could not be updated.");
+            return View(model);
+        }
         private MaterialService CreateMaterialService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
