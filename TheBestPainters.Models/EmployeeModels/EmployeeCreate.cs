b6cb07e36b9cae7f6a8129592f90aabc3eb30467
Luis Costilla <luis287@gmail.com>
2021-06-30 15:30:18 -0400

    Employee models added in model layer

---
+++ TheBestPainters.Models/EmployeeModels/EmployeeCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.EmployeeModels
+{
+    public class EmployeeCreate
+    {
+        public int? CrewId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string CityAddress { get; set; }
+
+        public string Email { get; set; }
+        public bool IsCrewChief { get; set; }
+    }
+}
---
+++ TheBestPainters.Models/EmployeeModels/EmployeeDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestPainters.Models.EmployeeModels
+{
+    public class EmployeeDetail
+    {
+        public int EmployeeId { get; set; }
+        public int CrewId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        [Display(Name = "City Address")]
+        public string CityAddress { get; set; }
+        public string Email { get; set; }
+        public bool IsCrewChief { get; set; }
+    }
+}
